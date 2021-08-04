e911449b85b5ab071b34442dfc43420f9ee809d9
tommy211369 <tommy.thongrasamy@gmail.com>
2021-08-04 19:21:54 +0200

    filter by name

--- routes/character.js
+++ routes/character.js
@@ -5,14 +5,22 @@ const axios = require("axios");
 // get all characters
 router.get("/characters", async (req, res) => {
   try {
+    let characterName = new RegExp("Abyss", "i");
+
     const response = await axios.get(
       `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.API_KEY}`
     );
 
     // list of characters from Le Reacteur API
-    let CHARACTERS = response.data.results;
+    const CHARACTERS = response.data.results;
+    console.log("Characters from API : ", CHARACTERS);
 
-    res.status(200).json(CHARACTERS);
+    const characters = await CHARACTERS.find(
+      (character) => character.name === characterName
+    );
+
+    console.log(characters);
+    res.status(200).json({ list: characters });
   } catch (error) {
     // console.log({ errorCharactersRouteMessage: error.message });
     res.status(400).json({ errorCharactersRouteMessage: error.message });
