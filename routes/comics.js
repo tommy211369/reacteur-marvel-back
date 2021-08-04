05588696e5812fff705468898fe8c0455cfd0da6
tommy211369 <tommy.thongrasamy@gmail.com>
2021-08-04 21:17:01 +0200

    filter comics

--- routes/comics.js
+++ routes/comics.js
@@ -5,11 +5,24 @@ const axios = require("axios");
 // get comics list
 router.get("/comics", async (req, res) => {
   try {
+    let title = req.query.title;
+    let regTitle = new RegExp(title, "i");
+
     const response = await axios.get(
       `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${process.env.API_KEY}`
     );
 
-    res.status(200).json(response.data.results);
+    // list of comics from Le Reacteur API
+    const COMICS = response.data.results;
+
+    //comics title filter
+    res.json(
+      COMICS.filter((comics) => {
+        return comics.title.search(regTitle) > -1 && comics;
+      })
+    );
+
+    // res.status(200).json(response.data.results);
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
