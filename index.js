1d07a4a2b9af6c4250f3f02d6b8eb5e708c5d14d
tommy211369 <tommy.thongrasamy@gmail.com>
2021-08-05 09:45:19 +0200

    add user models and routes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const formidable = require("express-formidable");
 const cors = require("cors");
 
@@ -7,11 +8,21 @@ const app = express();
 app.use(formidable());
 app.use(cors());
 
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
 // routes import
 const characterRoutes = require("./routes/character");
 const comicsRoutes = require("./routes/comics");
 app.use(characterRoutes);
 app.use(comicsRoutes);
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome on Marvel API !" });
+});
 
 app.all("*", (req, res) => {
   res.json(404).json({ message: "Page not found" });
---
+++ routes/user.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const router = express.Router();
+const uid2 = require("uid2");
+const SHA256 = require("crypto-js/sha256");
+const encBase64 = require("crypto-js/enc-base64");
+
+const User = require("../models/User");
+
+// SIGN UP
+router.post("/signup", async (req, res) => {
+  try {
+    const emailExist = await User.findOne({ email: req.fields.email });
+    if (emailExist) {
+      res.status(409).json({ message: "User already exists" });
+    } else if (req.fields.username === "") {
+      res.status(400).json({ message: "Username is required" });
+    } else {
+      const password = req.fields.password;
+      const userSalt = uid2(16);
+      const userHash = SHA256(password + userSalt).toString(encBase64);
+      const userToken = uid2(64);
+
+      const newUser = await new User({
+        email: req.fields.email,
+        username: req.fields.username,
+        token: userToken,
+        hash: userHash,
+        salt: userSalt,
+      });
+
+      await newUser.save();
+
+      const resNewUser = {
+        token: newUser.token,
+        username: newUser.username,
+      };
+
+      res.json({ message: "Signed up successfully", resNewUser });
+    }
+  } catch (error) {
+    res.status(400).json(error.message);
+  }
+});
+
+// LOGIN
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.fields.email });
+
+    if (user) {
+      const password = req.fields.password;
+      const newHash = SHA256(password + user.salt).toString(encBase64);
+
+      if (newHash === user.hash) {
+        const resUser = {
+          _id: user.id,
+          token: user.token,
+          account: user.account,
+        };
+
+        res.status(200).json({ message: "Logged in !", resUser: resUser });
+      } else {
+        res.status(400).json({ message: "Wrong password" });
+      }
+    } else {
+      res.status(401).json({ message: "Unauthorized : user not recognized" });
+    }
+  } catch (error) {
+    res.status(400).json(error.message);
+  }
+});
+
+module.exports = router;
